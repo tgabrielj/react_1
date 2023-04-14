994ae9c40ecfa97aead2bb714c460a1806beca9e
gjuncos <testingabriel@gmail.com>
2023-04-14 12:00:18 -0300

    hasta 0.52.53

--- react-course/src/index.js
+++ react-course/src/index.js
@@ -2,4 +2,21 @@ import React from "react";
 import  ReactDOM from "react-dom/client";
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
-root.render(<h1>Hello world</h1>)
+
+function Greeting(){
+    const user = {
+        firstName: "Joe",
+        lastname: "Mc Millan"
+    };
+    return(
+        <div>
+            <h1>{user.firstName}</h1>
+            <h1>{user.lastname}</h1>
+        </div>
+    )
+}
+root.render(
+    <div>
+        <Greeting/>
+    </div>
+)

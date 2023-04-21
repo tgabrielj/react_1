2defb01965d1c393cb28fd9f13700bd0712896e0
gjuncos <testingabriel@gmail.com>
2023-04-21 12:50:04 -0300

    ultimos cambios

--- react-course/src/index.js
+++ react-course/src/index.js
@@ -2,12 +2,19 @@ import React from "react";
 import  ReactDOM from "react-dom/client";
 import {Greeting,UserCard} from "./Greeting";
 import Product, {Navbar} from "./Product";
+import {Button} from './Button'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 
+// FUERA DEL HTML ESTO ES JS
+
 root.render(
     <div>
-        <Greeting title= "hola"/>
+        <Button text = 'Enviar' name = "gabriel"/>
+        <Button text = 'Salir'/>
+
+        <Button/>
+        {/* <Greeting title= "hola"/>
         <Greeting title= "bye"/>
         <Greeting title= {30}/>
         <Greeting title= {true}/>
@@ -21,6 +28,6 @@ root.render(
         greet = {function () { alert('Hello') }}
         />
         <Product/>
-        <Navbar/>
+        <Navbar/> */}
     </div>
 )
